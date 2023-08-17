a66f33211f4eb4b1654836869ba55ae097acddb4
Schmelzberry <jelsberry96@gmail.com>
2023-08-17 15:56:20 -0700

    append loop to webpage, WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
 // Business Logic
-
+let flavor;
 function renderFlava(flavor) {
   return flavor.split(" ");
+  
 }
 
 // UI Logic
  
-window.onload = function () {
+
   
-};
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("ice-cream").value;
+  const testArr = renderFlava(passage);
+  const ul = document.querySelector("ul#cream")
+  const li = document.createElement("li")
+  const appendLi = ul.append(li);
+  for (let i =0; i < testArr.length; i++) {
+      appendLi(testArr.split())
+      console.log(passage);
+  }
+}
 
-
-
+window.addEventListener("load", function() {
+   document.querySelector("form").addEventListener("submit", handleFormSubmission);
+});
 
 
 
